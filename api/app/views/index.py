2929cb7c0b9c460e8bc6464f18d4ac17879ec034
rickharris-dev <rickharris724@gmail.com>
2016-07-19 13:56:51 -0700

    Breaking files into views

---
+++ api/app/views/index.py
@@ -0,0 +1,14 @@
+from app import app
+
+# Controllers API
+@app.route("/ping")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+def ping():
+    return "All good. You don't need to be authenticated to call this"
+
+@app.route("/secured/ping")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@cross_origin(headers=['Access-Control-Allow-Origin', '*'])
+@requires_auth
+def securedPing():
+    return "All good. You only get this message if you're authenticated"
