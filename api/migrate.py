2c81e32c590b68d20a2e18b0693b5c88e6d04a19
Mason <mason.e.fish@gmail.com>
2016-07-19 12:37:59 -0700

    update migrate.py

--- api/migrate.py
+++ api/migrate.py
@@ -3,7 +3,8 @@ from app.models import *
 
 def create_tables():
     db_tables = [
-                ]
+            tweets.Tweet,
+            user.User]
 
     try:
         base.db.create_tables(db_tables, safe=True)
