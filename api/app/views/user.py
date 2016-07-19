c21a554904dd04b3be90411be0b8ee1ec616833e
Mason <mason.e.fish@gmail.com>
2016-07-19 16:32:10 -0700

    update

--- api/app/views/user.py
+++ api/app/views/user.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from flask import abort, jsonify
 
 @app.route('/users', methods=['POST'])
-@app.as_json
+@as_json
 def get_user():
     ''' Creates a new user '''
     data = request.get_json()
