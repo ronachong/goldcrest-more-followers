8f41e9d724077ad677bdf4fed96893ed20c175da
Mason <mason.e.fish@gmail.com>
2016-07-19 12:05:23 -0700

    push api

---
+++ api/app/models/user.py
@@ -0,0 +1,34 @@
+from peewee import *
+from base import BaseModel
+import hashlib
+from datetime import datetime
+
+'''
+User defines a user object which contains information
+about each user in the database.
+'''
+class User(BaseModel):
+    email = CharField(128, null=False, unique=True)
+    password = CharField(128, null=False)
+    name = CharField(128, null=False)
+    keyword = CharField(128, null=False)
+    is_active = BooleanField(default=False)
+    created_at = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    access_token = CharField(512, null=False)
+    access_token_secret = CharField(512, null=False)
+    
+    def to_hash(self):
+        hash = {
+            'user_id': self.user_id,
+            'created_at': self.created_at,
+            'email': self.email,
+            'name': self.name,
+            'is_active': self.is_admin,
+            'keyword': self.keyword,
+            'access_token': self.access_token,
+            'access_token_secret': self.access_token_secret
+        }
+
+        return hash
+        
+    
