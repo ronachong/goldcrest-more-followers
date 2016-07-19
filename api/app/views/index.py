56d1f01bb35930c82a36cdbec5de4b29dc5227ab
rickharris-dev <rickharris724@gmail.com>
2016-07-19 14:14:59 -0700

    Moved code from init to index

--- api/app/views/index.py
+++ api/app/views/index.py
@@ -6,6 +6,49 @@ from werkzeug.local import LocalProxy
 from dotenv import Dotenv
 from flask.ext.cors import cross_origin
 
+# Format error response and append status code.
+def handle_error(error, status_code):
+  resp = jsonify(error)
+  resp.status_code = status_code
+  return resp
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+      return handle_error({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != 'bearer':
+      return handle_error({'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}, 401)
+    elif len(parts) == 1:
+      return handle_error({'code': 'invalid_header', 'description': 'Token not found'}, 401)
+    elif len(parts) > 2:
+      return handle_error({'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}, 401)
+
+    token = parts[1]
+    try:
+        payload = jwt.decode(
+            token,
+            base64.b64decode(client_secret.replace("_","/").replace("-","+")),
+            audience=client_id
+        )
+    except jwt.ExpiredSignature:
+        return handle_error({'code': 'token_expired', 'description': 'token is expired'}, 401)
+    except jwt.InvalidAudienceError:
+        return handle_error({'code': 'invalid_audience', 'description': 'incorrect audience, expected: ' + client_id}, 401)
+    except jwt.DecodeError:
+        return handle_error({'code': 'token_invalid_signature', 'description': 'token signature is invalid'}, 401)
+    except Exception:
+        return handle_error({'code': 'invalid_header', 'description':'Unable to parse authentication token.'}, 400)
+
+    _request_ctx_stack.top.current_user = user = payload
+    return f(*args, **kwargs)
+
+  return decorated
+
 # Controllers API
 @app.route("/ping")
 @cross_origin(headers=['Content-Type', 'Authorization'])
