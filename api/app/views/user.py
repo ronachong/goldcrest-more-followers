04c952001ca5aecb128cb4ca45053ac3e2aabbb7
rickharris-dev <rickharris724@gmail.com>
2016-07-19 15:45:00 -0700

    Updated test route

--- api/app/views/user.py
+++ api/app/views/user.py
@@ -7,7 +7,7 @@ from flask import abort
 
 @app.route('/users', methods=['POST'])
 @as_json
-def get_user(user_id):
+def get_user():
     ''' Creates a new user '''
     data = request.get_json()
     return {"message": "Hello World"}, 200
